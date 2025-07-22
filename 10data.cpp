3ac574a47dcc4d973e7d5aa506800f7c26fc4d59
Krish Ramena <krish.ramena24@vit.edu>
2025-07-22 11:25:25 +0530

    Add files via upload

---
+++ 10data.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+struct Student {
+    char email[100];
+    char number[20];
+    char rollno[8];
+    char name[100];
+    float marks;
+};
+
+int main(){
+    Student data[10];
+
+    for(int i = 0; i < 10; i++){
+        cout << "\nEnter the name of the student: ";
+        cin >> data[i].name;
+
+        for(int j = 0; j < strlen(data[i].name); j++){
+            if(data[i].name[j] >= '0' && data[i].name[j] <= '9') {
+                cout << "Invalid name.\n";
+                return -1;
+            }
+        }
+
+        cout << "\nEnter the rollno: ";
+        cin >> data[i].rollno;
+
+        for(int j = 0; j < strlen(data[i].rollno); j++){
+            if(!(data[i].rollno[j] >= '0' && data[i].rollno[j] <= '9')) {
+                cout << "Invalid roll number.\n";
+                return -1;
+            }
+        }
+
+        cout << "\nEnter the marks of the student: ";
+        cin >> data[i].marks;
+        if(data[i].marks > 100 || data[i].marks < 0) {
+            cout << "Invalid marks.\n";
+            return -1;
+        }
+
+        cout << "\nEnter the email of the student: ";
+        cin >> data[i].email;
+
+        char sum[100] = "";
+        int k = 0;
+        int len = strlen(data[i].email);
+        if(len < 8) {
+            cout << "Invalid email.\n";
+            return -1;
+        }
+        for(int j = 8; j > 0; j--) {
+            sum[k++] = data[i].email[len - j];
+        }
+        sum[k] = '\0';
+
+        if(strcmp(sum, "@vit.edu") != 0) {
+            cout << "Invalid email domain.\n";
+            return -1;
+        }
+
+        cout << "\nEnter the mob. number of the student: ";
+        cin >> data[i].number;
+        if(strlen(data[i].number) != 10) {
+            cout << "Invalid mobile number length.\n";
+            return -1;
+        }
+        for(int j = 0; j < 10; j++){
+            if(!(data[i].number[j] >= '0' && data[i].number[j] <= '9')) {
+                cout << "Invalid mobile number.\n";
+                return -1;
+            }
+        }
+
+        cout << endl;
+    }
+
+    for(int i = 0; i < 10; i++){
+        cout << "\nStudent " << i+1 << endl;
+        cout << "\tStudent Name: " << data[i].name << endl;
+        cout << "\tStudent Rollno: " << data[i].rollno << endl;
+        cout << "\tStudent Marks: " << data[i].marks << endl;
+        cout << "\tStudent Email: " << data[i].email << endl;
+        cout << "\tStudent Phone: " << data[i].number << endl;
+    }
+    return 0;
+}
