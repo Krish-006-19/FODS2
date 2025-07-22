c63d7d1a2a2b4407dbb2d645ac695596b9c23f2e
Krish Ramena <krish.ramena24@vit.edu>
2025-07-22 14:20:33 +0530

    Update 10data.cpp

--- 10data.cpp
+++ 10data.cpp
@@ -10,76 +10,100 @@ struct Student {
     float marks;
 };
 
-int main(){
+int main() {
     Student data[10];
 
-    for(int i = 0; i < 10; i++){
-        cout<<"Student"<<i+1<<endl;
-        cout << "\nEnter the name of the student as (name_surename): ";
-        cin >> data[i].name;
-
-        for(int j = 0; j < strlen(data[i].name); j++){
-            if(data[i].name[j] >= '0' && data[i].name[j] <= '9') {
-                cout << "Invalid name.\n";
-                return -1;
+    for(int i = 0; i < 10; i++) {
+        cout << "Student " << i + 1 << endl;
+        
+        bool validName = false;
+        while (!validName) {
+            cout << "\nEnter the name of the student as (name_surename): ";
+            cin >> data[i].name;
+            
+            validName = true;
+            for(int j = 0; j < strlen(data[i].name); j++) {
+                if(data[i].name[j] >= '!' && data[i].name[j] <= '@') {
+                    cout << "Invalid name. Please enter a valid name.\n";
+                    validName = false;
+                    break;
+                }
             }
         }
 
-        cout << "\nEnter the rollno: ";
-        cin >> data[i].rollno;
-
-        for(int j = 0; j < strlen(data[i].rollno); j++){
-            if(!(data[i].rollno[j] >= '0' && data[i].rollno[j] <= '9')) {
-                cout << "Invalid roll number.\n";
-                return -1;
+        bool validRollno = false;
+        while (!validRollno) {
+            cout << "\nEnter the rollno: ";
+            cin >> data[i].rollno;
+            validRollno = true;
+            for(int j = 0; j < strlen(data[i].rollno); j++) {
+                if(!(data[i].rollno[j] >= '0' && data[i].rollno[j] <= '9')) {
+                    cout << "Invalid roll number. Please enter a valid roll number.\n";
+                    validRollno = false;
+                    break;
+                }
             }
         }
 
-        cout << "\nEnter the marks of the student: ";
-        cin >> data[i].marks;
-        if(data[i].marks > 100 || data[i].marks < 0) {
-            cout << "Invalid marks.\n";
-            return -1;
-        }
-
-        cout << "\nEnter the email of the student: ";
-        cin >> data[i].email;
-
-        char sum[100] = "";
-        int k = 0;
-        int len = strlen(data[i].email);
-        if(len < 8) {
-            cout << "Invalid email.\n";
-            return -1;
-        }
-        for(int j = 8; j > 0; j--) {
-            sum[k++] = data[i].email[len - j];
-        }
-        sum[k] = '\0';
-
-        if(strcmp(sum, "@vit.edu") != 0) {
-            cout << "Invalid email domain.\n";
-            return -1;
-        }
-
-        cout << "\nEnter the mob. number of the student: ";
-        cin >> data[i].number;
-        if(strlen(data[i].number) != 10) {
-            cout << "Invalid mobile number length.\n";
-            return -1;
-        }
-        for(int j = 0; j < 10; j++){
-            if(!(data[i].number[j] >= '0' && data[i].number[j] <= '9')) {
-                cout << "Invalid mobile number.\n";
-                return -1;
+        bool validMarks = false;
+        while (!validMarks) {
+            cout << "\nEnter the marks of the student: ";
+            cin >> data[i].marks;
+            if(data[i].marks > 100 || data[i].marks < 0) {
+                cout << "Invalid marks. Please enter marks between 0 and 100.\n";
+            } else {
+                validMarks = true;
             }
         }
 
+        bool validEmail = false;
+        while (!validEmail) {
+            cout << "\nEnter the email of the student: ";
+            cin >> data[i].email;
+
+            char sum[100] = "";
+            int k = 0;
+            int len = strlen(data[i].email);
+            if(len < 8) {
+                cout << "Invalid email. Please enter a valid email.\n";
+                continue; 
+            }
+
+            for(int j = 8; j > 0; j--) {
+                sum[k++] = data[i].email[len - j];
+            }
+            sum[k] = '\0';
+
+            if(strcmp(sum, "@vit.edu") != 0) {
+                cout << "Invalid email domain. Please enter a valid email with @vit.edu domain.\n";
+            } else {
+                validEmail = true;
+            }
+        }
+
+        bool validNumber = false;
+        while (!validNumber) {
+            cout << "\nEnter the mob. number of the student: ";
+            cin >> data[i].number;
+            if(strlen(data[i].number) != 10) {
+                cout << "Invalid mobile number length. Please enter a 10-digit mobile number.\n";
+            } else {
+                validNumber = true;
+                for(int j = 0; j < 10; j++) {
+                    if(!(data[i].number[j] >= '0' && data[i].number[j] <= '9')) {
+                        cout << "Invalid mobile number. Please enter a valid mobile number.\n";
+                        validNumber = false;
+                        break;
+                    }
+                }
+            }
+        }
+        
         cout << endl;
     }
 
-    for(int i = 0; i < 10; i++){
-        cout << "\nStudent " << i+1 << endl;
+    for(int i = 0; i < 10; i++) {
+        cout << "\nStudent " << i + 1 << endl;
         cout << "\tStudent Name: " << data[i].name << endl;
         cout << "\tStudent Rollno: " << data[i].rollno << endl;
         cout << "\tStudent Marks: " << data[i].marks << endl;
